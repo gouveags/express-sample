bd4b0581be650a7a8657cf6ce795bb583c921f91
Daniel dos Santos Fonseca <daniel.fonseca@ip.tv>
2024-07-13 17:13:08 -0300

    fix: configs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configureOpenAPI } from '@app/config';
+import { configureOpenAPI } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule).then(configureOpenAPI);
